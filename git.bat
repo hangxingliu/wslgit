b6a5dbe295a93ba799848232745ee0e134d9734c
Liu Yue <hangxingliu@gmail.com>
2019-02-06 23:15:52 +0800

    fixed error when calling git.bat without arguments

--- git.bat
+++ git.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem  Update: 2018-07-27
+rem  Update: 2019-02-06
 rem  Author: Liu Yue (hangxingliu@gmail.com)
 rem
 rem  Description:
@@ -14,10 +14,24 @@ rem        set "VAR=%VAR:REPLACE_FROM=REPLACE_TO%"
 rem        Chinese blog: https://www.jb51.net/article/110243.htm
 rem   2. Why prepend  `setlocal enabledelayedexpansion`
 rem        Stackoverflow: https://stackoverflow.com/questions/6679907
+rem   3. How to detect a variable is empty:
+rem        Stackoverflow: https://stackoverflow.com/a/2541820/3831547
+rem   4. Get command exit code: (Just like "$?" on linux)
+rem        Stackoverflow: https://stackoverflow.com/a/334890/3831547
+rem
+rem
 
 setlocal enabledelayedexpansion
 
-set args=%*
-set "args=%args:\=\\%"
+if [%1] == [] goto WITHOUT_ARGS
 
-wsl wslgit.sh %args%
+:WITH_ARGS
+	set args=%*
+	set "args=%args:\=\\%"
+
+	wsl wslgit.sh %args%
+	exit /b %errorlevel%
+
+:WITHOUT_ARGS
+	wsl wslgit.sh
+	exit /b %errorlevel%
