9bcf4af8ece9f0f03cee8bb7a8b771d0409df2f4
Liu Yue <hangxingliu@gmail.com>
2019-03-11 19:18:40 +0800

    Add support of Windows env variable: `WSLGIT_USE_INTERACTIVE_SHELL` #12

--- git.bat
+++ git.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem  Update: 2019-02-06
+rem  Update: 2019-03-11
 rem  Author: Liu Yue (hangxingliu@gmail.com)
 rem
 rem  Description:
@@ -25,15 +25,29 @@ setlocal enabledelayedexpansion
 
 set "currentdir=%cd:\=\\%"
 
+rem Enable interactive mode by default.
+rem And disable it by set Windows environment variale WSLGIT_USE_INTERACTIVE_SHELL to 0 or false
+set "bashic=true"
+if [%WSLGIT_USE_INTERACTIVE_SHELL%] == [0] set bashic=false
+if [%WSLGIT_USE_INTERACTIVE_SHELL%] == [false] set bashic=false
+
 if [%1] == [] goto WITHOUT_ARGS
 
 :WITH_ARGS
 	set args=%*
 	set "args=%args:\=\\%"
 
-	wsl bash -ic 'env "WSLGIT_SH_CWD=%currentdir%" wslgit.sh %args%'
+	if %bashic% == true (
+		wsl bash -ic 'env "WSLGIT_SH_CWD=%currentdir%" wslgit.sh %args%'
+	) else (
+		wsl env "WSLGIT_SH_CWD=%currentdir%" wslgit.sh %args%
+	)
 	exit /b %errorlevel%
 
 :WITHOUT_ARGS
-	wsl bash -ic 'env "WSLGIT_SH_CWD=%currentdir%" wslgit.sh'
+	if %interactive% == true (
+		wsl bash -ic 'env "WSLGIT_SH_CWD=%currentdir%" wslgit.sh'
+	) else (
+		wsl env "WSLGIT_SH_CWD=%currentdir%" wslgit.sh
+	)
 	exit /b %errorlevel%
